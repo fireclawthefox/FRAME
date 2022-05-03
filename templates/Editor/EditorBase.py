486d018aa15d8620d8e36cd9f23a4d03da3945be
Fireclaw <fireclawthefox@gmail.com>
2022-05-03 20:51:36 +0200

    Add a template for new FRAME embedable editors

---
+++ templates/Editor/EditorBase.py
@@ -0,0 +1,31 @@
+class Editor:
+    def __init__(self, parent):
+        # Initial setup of the Editor
+
+    def is_dirty(self):
+        """
+        This method returns True if an unsaved state of the editor is given
+        """
+        return False
+
+    def enable_editor(self):
+        """
+        Enable the editor.
+        """
+        # Do things like catching events, enabling tasks, etc.
+        pass
+
+    def disable_editor(self):
+        """
+        Disable the editor.
+        """
+        # Do things like ignoring events, stop tasks, etc.
+        pass
+
+    def do_exception_save(self):
+        """
+        Save content of editor if the application crashes
+        """
+        # Save all data, preferably in a crash save file to prevent overriding
+        # existing saves with corrupted data.
+        pass
