2f5f5df9c37d25f469f737a60cf8be8724930a7f
Fireclaw <fireclawthefox@gmail.com>
2022-06-23 16:55:33 +0200

    fix node editor extension if editor is not installed
    check if imports succeed, otherwise do not try to extend the node editor

--- Frame/Extensions/NodeEditor/NodeEditorExtender.py
+++ Frame/Extensions/NodeEditor/NodeEditorExtender.py
@@ -1,14 +1,22 @@
-from Frame.Extensions.NodeEditor.Nodes.P3DEventNode import Node as P3DEventNode
-from Frame.Extensions.NodeEditor.Nodes.P3DCallScriptNode import Node as P3DCallScriptNode
-from Frame.Extensions.NodeEditor.Nodes.P3DScriptLoaderNode import Node as P3DScriptLoaderNode
-from Frame.Extensions.NodeEditor.Exporters.PythonExporter import PythonExporter
+
+try:
+    from Frame.Extensions.NodeEditor.Nodes.P3DEventNode import Node as P3DEventNode
+    from Frame.Extensions.NodeEditor.Nodes.P3DCallScriptNode import Node as P3DCallScriptNode
+    from Frame.Extensions.NodeEditor.Nodes.P3DScriptLoaderNode import Node as P3DScriptLoaderNode
+    from Frame.Extensions.NodeEditor.Exporters.PythonExporter import PythonExporter
+    NODE_EDITOR_AVAILABLE = True
+except:
+    NODE_EDITOR_AVAILABLE = False
 
 class NodeEditorExtender:
     def get_frame_logic_node_menu(self):
-        return [
-            {"Logic >":{
-                "Catch Event": ["P3DEventNode", P3DEventNode],
-                "Load Script": ["P3DScriptLoaderNode", P3DScriptLoaderNode],
-                "Call Function": ["P3DCallScriptNode", P3DCallScriptNode]
-            }},
-            {"Export Python Script": PythonExporter}]
+        if NODE_EDITOR_AVAILABLE:
+            return [
+                {"Logic >":{
+                    "Catch Event": ["P3DEventNode", P3DEventNode],
+                    "Load Script": ["P3DScriptLoaderNode", P3DScriptLoaderNode],
+                    "Call Function": ["P3DCallScriptNode", P3DCallScriptNode]
+                }},
+                {"Export Python Script": PythonExporter}]
+        else:
+            return []
