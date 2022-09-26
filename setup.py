c274a07bb85897cfc0f0f661864c3370aa5d7e46
Fireclaw <fireclawthefox@gmail.com>
2022-09-26 16:46:39 +0200

    typo in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pand3d-frame",
+    name="panda3d-frame",
     version="22.09",
     author="Fireclaw",
     author_email="fireclawthefox@gmail.com",
