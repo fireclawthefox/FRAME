0042fe06f70ea29964cd476949fc5d130625e94a
Fireclaw <fireclawthefox@gmail.com>
2022-09-26 16:45:25 +0200

    fixes for deployment
    fix paths (can't use base.main_dir as that would be relative to the bin
    folder, not the package folder. Revert back to __file__ as we are not
    building using panda3ds packaging which would prevent __file__ from
    working.
    
    Include missing files in manifest

--- panda3d_frame/core/FrameProject.py
+++ panda3d_frame/core/FrameProject.py
@@ -7,12 +7,12 @@ import json
 from datetime import datetime
 from panda3d.core import Filename
 
-#FRAME_ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+FRAME_ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 class FrameProject:
     def __init__(self):
         main_dir = Filename(base.main_dir).to_os_specific()
-        self.template_path = os.path.join(main_dir, "panda3d_frame", "templates")
+        self.template_path = os.path.join(FRAME_ROOT_PATH, "templates")
         self.project_path = ""
         self.game_name = ""
         self.company_name = ""
